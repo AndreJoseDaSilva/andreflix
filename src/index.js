839eef51b52f4d4f93a3c3d6824cd69012bddf45
AndreJoseDaSilva <andre.jose.da.silva.jdi@gmail.com>
2020-07-31 18:07:17 -0300

    Implementado o Router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+const Pagina404 = () => (<div>Página 404</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+   <BrowserRouter>
+      <Switch>
+         <Route path="/cadastro/video" component={CadastroVideo} exact />
+         <Route path="/cadastro/categoria" component={CadastroCategoria} exact />
+         <Route path="/" component={Home} exact />
+         <Route component={Pagina404} />  {/* Se não encontrar nenhuma rota, então mostra isso. */}
+      </Switch>
+   </BrowserRouter>,
+   document.getElementById('root')
 );
 
